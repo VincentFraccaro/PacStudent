8254f872d88b9b828a77bf05ad4aa049553fe79d
VincentFraccaro <vincentfraccaro@gmail.com>
2023-10-04 09:01:57 +1100

    Basic manual level loader implemented. Doesn't rotate position of sprites yet

---
+++ Assets/Scripts/ManualLevel.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualLevel : MonoBehaviour
+{
+    [SerializeField] private GameObject[] spriteList;
+    
+    private int[,] levelMap =
+    {
+        { 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7 },
+        { 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4 },
+        { 2, 5, 3, 4, 4, 3, 5, 3, 4, 4, 4, 3, 5, 4 },
+        { 2, 6, 4, 0, 0, 4, 5, 4, 0, 0, 0, 4, 5, 4 },
+        { 2, 5, 3, 4, 4, 3, 5, 3, 4, 4, 4, 3, 5, 3 },
+        { 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
+        { 2, 5, 3, 4, 4, 3, 5, 3, 3, 5, 3, 4, 4, 4 },
+        { 2, 5, 3, 4, 4, 3, 5, 4, 4, 5, 3, 4, 4, 3 },
+        { 2, 5, 5, 5, 5, 5, 5, 4, 4, 5, 5, 5, 5, 4 },
+        { 1, 2, 2, 2, 2, 1, 5, 4, 3, 4, 4, 3, 0, 4 },
+        { 0, 0, 0, 0, 0, 2, 5, 4, 3, 4, 4, 3, 0, 3 },
+        { 0, 0, 0, 0, 0, 2, 5, 4, 4, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 2, 5, 4, 4, 0, 3, 4, 4, 0 },
+        { 2, 2, 2, 2, 2, 1, 5, 3, 3, 0, 4, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 4, 0, 0, 0 },
+    };
+    
+    void LoadLevel()
+    {
+        print("Yoyo");
+        for (int i = 0; i < levelMap.GetLength(0); i++)
+        {
+            for (int j = 0; j < levelMap.GetLength(1); j++)
+            {
+                int tileIndex = levelMap[i, j];
+                if(tileIndex == 0) continue; 
+                if(tileIndex > 0 && tileIndex < spriteList.Length) 
+                {
+                    Instantiate(spriteList[tileIndex], new Vector3(j, -i, 0), Quaternion.identity, this.transform); 
+                }
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
